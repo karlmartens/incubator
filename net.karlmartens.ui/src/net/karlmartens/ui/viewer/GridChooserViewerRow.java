9f04549511822a67752381f8936395d1d88eb844
Karl Martens <karl@karlmartens.net>
2011-03-30 07:44:58 -0600

    Converted net.karlmatens.ui to Apache 2.0 License

--- net.karlmartens.ui/src/net/karlmartens/ui/viewer/GridChooserViewerRow.java
+++ net.karlmartens.ui/src/net/karlmartens/ui/viewer/GridChooserViewerRow.java
@@ -1,23 +1,24 @@
 /**
- *  net.karlmartens.ui, is a library of UI widgets
- *  Copyright (C) 2010,2011
- *  Karl Martens
+ *   Copyright 2010,2011 Karl Martens
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as 
- *  published by the Free Software Foundation, either version 3 of the 
- *  License, or (at your option) any later version.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *       
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *   net.karlmartens.ui, is a library of UI widgets
  */
 package net.karlmartens.ui.viewer;
+
+import net.karlmartens.ui.widget.GridChooser;
+import net.karlmartens.ui.widget.GridChooserItem;
 
 import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.ViewerRow;
@@ -27,9 +28,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Widget;
-
-import net.karlmartens.ui.widget.GridChooser;
-import net.karlmartens.ui.widget.GridChooserItem;
 
 final class GridChooserViewerRow extends ViewerRow {
 
