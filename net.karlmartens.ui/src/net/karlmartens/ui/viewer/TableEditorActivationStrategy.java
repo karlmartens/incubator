23bb523cef8c1b4edb179b931f9cb4825db5241a
Karl Martens <kmartens@3esi.com>
2011-08-31 11:57:31 -0600

    #55 Table columns with a checkbox should toggle check state with a single click.

--- net.karlmartens.ui/src/net/karlmartens/ui/viewer/TableEditorActivationStrategy.java
+++ net.karlmartens.ui/src/net/karlmartens/ui/viewer/TableEditorActivationStrategy.java
@@ -19,26 +19,40 @@ package net.karlmartens.ui.viewer;
 
 import net.karlmartens.ui.widget.CellNavigationStrategy;
 import net.karlmartens.ui.widget.ClipboardStrategy;
+import net.karlmartens.ui.widget.TableColumn;
+import net.karlmartens.ui.widget.TableItem;
 
 import org.eclipse.jface.viewers.ColumnViewerEditorActivationEvent;
 import org.eclipse.jface.viewers.ColumnViewerEditorActivationStrategy;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Event;
+
+import de.kupzog.ktable.renderers.CheckableCellRenderer;
 
 final class TableEditorActivationStrategy extends ColumnViewerEditorActivationStrategy {
 
   private final CellNavigationStrategy _navigationStrategy = new CellNavigationStrategy();
   private final ClipboardStrategy _clipboardStrategy = new ClipboardStrategy();
+  private final TableViewer _viewer;
 
   public TableEditorActivationStrategy(TableViewer viewer) {
     super(viewer);
+    _viewer = viewer;
   }
 
   @Override
   protected boolean isEditorActivationEvent(ColumnViewerEditorActivationEvent event) {
-    if (event.eventType != ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION && event.eventType != ColumnViewerEditorActivationEvent.KEY_PRESSED
-        && event.eventType != ColumnViewerEditorActivationEvent.PROGRAMMATIC)
+    if (event.eventType != ColumnViewerEditorActivationEvent.MOUSE_CLICK_SELECTION
+        && event.eventType != ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION
+        && event.eventType != ColumnViewerEditorActivationEvent.KEY_PRESSED && event.eventType != ColumnViewerEditorActivationEvent.PROGRAMMATIC)
       return false;
+
+    if (event.eventType == ColumnViewerEditorActivationEvent.MOUSE_CLICK_SELECTION)
+      return isCheckActivationEvent(event);
 
     if (event.eventType == ColumnViewerEditorActivationEvent.KEY_PRESSED) {
       final Event e = new Event();
@@ -66,5 +80,42 @@ final class TableEditorActivationStrategy extends ColumnViewerEditorActivationSt
     }
 
     return false;
+  }
+
+  private boolean isCheckActivationEvent(ColumnViewerEditorActivationEvent event) {
+    if (event.eventType != ColumnViewerEditorActivationEvent.MOUSE_CLICK_SELECTION)
+      return false;
+
+    final Object source = event.getSource();
+    if (!(source instanceof ViewerCell))
+      return false;
+
+    final ViewerCell cell = (ViewerCell) source;
+    final int columnIdx = cell.getColumnIndex();
+    final TableColumn column = _viewer.getControl().getColumn(columnIdx);
+    final int style = column.getStyle();
+    if ((style & SWT.CHECK) == 0)
+      return false;
+
+    final MouseEvent mEvent = (MouseEvent) event.sourceEvent;
+    final Point eventPt = new Point(mEvent.x, mEvent.y);
+    final TableItem item = _viewer.getItemAt(eventPt);
+
+    final Rectangle cellBounds = item.getBounds(columnIdx);
+    final Rectangle targetBounds = CheckableCellRenderer.IMAGE_CHECKED.getBounds();
+
+    final Rectangle activationBounds = new Rectangle(cellBounds.x, cellBounds.y, targetBounds.width, targetBounds.height);
+    activationBounds.y += Math.max((cellBounds.height - targetBounds.height) / 2, 0);
+
+    if ((style & SWT.RIGHT) > 0) {
+      activationBounds.x += Math.max(cellBounds.width - targetBounds.width, 0);
+    } else if ((style & SWT.LEFT) > 0) {
+      // nothing to do
+    } else {
+      // default is centered
+      activationBounds.x += Math.max((cellBounds.width - targetBounds.width) / 2, 0);
+    }
+
+    return activationBounds.contains(eventPt);
   }
 }
